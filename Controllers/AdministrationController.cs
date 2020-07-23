29d64b2fd6c29849e156c9c08e55df93f135bd0b
Ritesh Hanmanthu <ac94848@avcol.school.nz>
2020-07-23 15:15:18 +1200

    Error Fixed. URL file path works

---
+++ Controllers/AdministrationController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Permissions;
+using System.Threading.Tasks;
+using LowVision.Views.AdministrationRoles;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace LowVision.Controllers
+{
+   public   class AdministrationController : Controller
+    {
+        private readonly RoleManager<IdentityRole> roleManager;
+
+        public AdministrationController(RoleManager<IdentityRole>roleManager)
+        {
+            this.roleManager = roleManager;
+        }
+
+        [HttpGet]
+        public IActionResult CreateRole()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public async Task <IActionResult> CreateRole(CreateRoleViewModel model)
+        {
+            if(ModelState.IsValid)
+            {
+                IdentityRole identityRole = new IdentityRole
+                {
+                    Name = model.RoleName
+                };
+
+                IdentityResult result = await roleManager.CreateAsync(identityRole);
+
+                if(result.Succeeded)
+                {
+                    return RedirectToAction("index", "home");
+                }
+
+                foreach(IdentityError error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+            }
+
+            return View();
+        }
+                
+ 
+    }
+}
